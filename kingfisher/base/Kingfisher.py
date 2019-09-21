8fa7e7adbc4d3d73174465eadac11e2da99d3971
NaGeL <nagel182@gmail.com>
2019-09-21 17:28:41 +0200

    logger finally works how i want.

--- kingfisher/base/Kingfisher.py
+++ kingfisher/base/Kingfisher.py
@@ -1,4 +1,13 @@
+import discord
 from discord.ext.commands import Bot
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from logging import StreamHandler
+from sys import stdout
+from pprint import pprint
+import os
+import yaml
+from logging import config as log_config
 
 
 class KingfisherBot(Bot):
@@ -6,3 +15,17 @@ class KingfisherBot(Bot):
         super().__init__(command_prefix, **options)
         self.db = db
         self.config = config
+        self._setup_logging()
+
+    @staticmethod
+    def _setup_logging():
+        # everything about logging
+        # https://docs.python.org/3.7/library/logging.html
+        """
+        Basically I'm logging the yaml file with logging configuration so we use that.
+        """
+        path = os.path.dirname(os.path.realpath(__file__))
+        logging_conf = open(f"{path}/../config/logging.yaml", 'r')
+        log_config.dictConfig(yaml.load(logging_conf, Loader=yaml.SafeLoader))
+
+
--- kingfisher/kingfisher.py
+++ kingfisher/kingfisher.py
@@ -1,9 +1,5 @@
 from config import config
-from discord.ext.commands import Bot
-import logging
-from logging.handlers import TimedRotatingFileHandler
-from logging import StreamHandler
-from sys import stdout
+from base.Kingfisher import KingfisherBot
 
 major_version = 0
 minor_version = 3
@@ -13,41 +9,22 @@ version = f"{major_version}.{minor_version}.{patch_version}"
 
 
 def create_bot():
-    return Bot(
+    db = {}
+    return KingfisherBot(
         description=f"Thinkerbot version {version}",
         command_prefix=config.command_prefix,
         case_insensitive=True,
-        owner_id=config.owner
+        owner_id=config.owner,
+        db=db,
+        config=config,
     )
 
 
-def setup_logging():
-    # everything about logging
-    # https://docs.python.org/3.7/library/logging.html
-    formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
-
-    file_handler = TimedRotatingFileHandler(
-        filename=config.log['file'],
-        when=config.log['when'],
-        interval=config.log['interval'],
-        backupCount=config.log['backupCount'],
-        utc=config.log['utc']
-    )
-    file_handler.setFormatter(formatter)
-
-    console_handler = StreamHandler(stdout)
-    console_handler.setFormatter(formatter)
-
-    root_logger = logging.getLogger()
-    # if we set it here we set all the logger even imported modules log level
-    # but right now i dont know better ...
-    root_logger.setLevel(config.log['level'])
-    root_logger.addHandler(file_handler)
-    root_logger.addHandler(console_handler)
-
-
-if __name__ == '__main__':
-    setup_logging()
+def main():
     bot = create_bot()
     bot.load_extension('base')
     bot.run(config.token)
+
+
+if __name__ == '__main__':
+    main()
